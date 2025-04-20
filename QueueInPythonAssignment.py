8a9f8116764faded2daa2ff6b24155966d5796b2
Weeeeeam <william.ryan.christianson@gmail.com>
2025-04-20 10:45:28 -0700

    got code working with  Queue.Queue

--- QueueInPythonAssignment.py
+++ QueueInPythonAssignment.py
@@ -1,23 +1,43 @@
-from queue import Queue    
-def SortIntoList():
-    Priority = Queue(maxsize = 10)
-    Standard = Queue(maxsize = 10)
-    Economy = Queue(maxsize = 10)
+from queue import Queue   
+Priority = Queue(maxsize = 10)
+Standard = Queue(maxsize = 10)
+Economy = Queue(maxsize = 10)
+def LoadData():
     X=['S Mary','P Dee','P Dee','E Eileen','E Mike','E Joe','P Dee','E Vicky','E George','P Dee','P Joe','E Sally','P Joe','S Pete','P Dee','S Bill','S Chase','E Price','P Dee','E Sue']
-    while X[0]!= ' ':
-        y=0
-        InputLists= X[y]
-        if InputLists[0]=='P':
-            Priority.put(InputLists)
-            y=y+1
-            print(list(Priority.queue))
-        elif InputLists[0]=='S':
-            Standard.put(InputLists)
-            y=y+1
-        elif InputLists[0]=='E':
-            Economy.put(InputLists)
-            y=y+1
-        else:
-            exit
+    return X
+def SortIntoList():
+    X=LoadData()
+    
+    y=0
+    while y<len(X):
+        Item= X[y]
+        if Item[0]=='P':
+            Priority.put(Item)
+        elif Item[0]=='S':
+            Standard.put(Item)
+        elif Item[0]=='E':
+            Economy.put(Item)
+        y=y+1
     print(list(Economy.queue))
+    print(list(Priority.queue))
+    print(list(Standard.queue))
+   
+
+
+def EmptyQueue():
+    while Priority.empty()!=True or Standard.empty()!=True or Economy.empty()!=True:
+        if  Priority.empty()!=True:
+            print(Priority.get())
+        if  Priority.empty()!=True:   
+            print(Priority.get())
+        if  Priority.empty()!=True:
+            print(Priority.get())
+        if Economy.empty()!=True:
+            print(Economy.get())
+        if Economy.empty()!=True:
+            print(Economy.get())
+        if Standard.empty()!=True:
+            print(Standard.get())
+
 SortIntoList()
+EmptyQueue()
