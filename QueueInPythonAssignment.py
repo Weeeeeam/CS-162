f52a2d883d561c4b9fc975cb6ef273c8e07c7a8e
Weeeeeam <william.ryan.christianson@gmail.com>
2025-04-20 09:43:39 -0700

    Update QueueInPythonAssignment.py

--- QueueInPythonAssignment.py
+++ QueueInPythonAssignment.py
@@ -1,8 +1,8 @@
 from queue import Queue    
 def SortIntoList():
-    Priority = Queue(maxsize = 0)
-    Standard = Queue(maxsize = 0)
-    Economy = Queue(maxsize = 0)
+    Priority = Queue(maxsize = 10)
+    Standard = Queue(maxsize = 10)
+    Economy = Queue(maxsize = 10)
     X=['S Mary','P Dee','P Dee','E Eileen','E Mike','E Joe','P Dee','E Vicky','E George','P Dee','P Joe','E Sally','P Joe','S Pete','P Dee','S Bill','S Chase','E Price','P Dee','E Sue']
     while X[0]!= ' ':
         y=0
@@ -10,6 +10,7 @@ def SortIntoList():
         if InputLists[0]=='P':
             Priority.put(InputLists)
             y=y+1
+            print(list(Priority.queue))
         elif InputLists[0]=='S':
             Standard.put(InputLists)
             y=y+1
@@ -17,6 +18,6 @@ def SortIntoList():
             Economy.put(InputLists)
             y=y+1
         else:
-            print(list(Economy.queue))
             exit
+    print(list(Economy.queue))
 SortIntoList()
